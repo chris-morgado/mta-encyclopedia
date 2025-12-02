5f374dbebc8831a22bfdd6edd4b48e2c125825f6
Chris Morgado <cmmorgado53@gmail.com>
2025-12-02 11:36:15 -0500

    preprocessing stub

--- mtae-frontend/src/data/routes.tsx
+++ mtae-frontend/src/data/routes.tsx
@@ -6,5 +6,30 @@ const ROUTES_URL = "/data/mta-routes.geojson";
 export async function fetchRoutesGeoJson(): Promise<RouteGeoJson> {
     const res = await fetch(ROUTES_URL);
     if (!res.ok) throw new Error("Failed to load routes GeoJSON");
-    return res.json();
+    const raw = await res.json();
+    const preprocessedData = preprocessRoutes(raw);
+    return raw;
+}
+
+/**
+ * Methodology:
+ * 
+ * 1: Find all lines that share multiple routes (same geometry)
+ * 2: For each group of shared-geometry routes:
+ *  a) If all routes have the same color, merge into one 'trunk' line with no offset
+ *  b) If multiple colors (2+), create offset lines PER COLOR (not per route)
+ * 
+ *  
+ */
+
+
+
+
+function preprocessRoutes(raw: RouteGeoJson): RouteGeoJson {
+	const geomMap = new Map<string, RouteFeature[]>();
+
+	return {
+		...raw,
+        features: raw.features,
+	};
 }
